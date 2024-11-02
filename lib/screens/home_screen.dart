3fd03924a63a21d7ee72ef28dc4fb4ec769f0cb4
OmAlKml <omar.ali.kml@gmail.com>
2024-11-02 21:18:14 +0200

    Add home_screen.dart

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,68 @@
+import 'package:calculator/screens/calc_screen.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF6A5ACD), // Slate Blue
+              Color(0xFF00CED1), // Dark Turquoise
+            ], // Top to bottom gradient
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Stack(children: [
+          _screenContent(),
+        ]),
+      ),
+    );
+  }
+
+  Widget _screenContent() {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Welcome to the calculator app",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                fontFamily: "cairo",
+              ),
+            ),
+            const SizedBox(height: 25.0),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to CalculatorScreen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const CalculatorScreen()),
+                );
+              },
+              child: const Text("Start"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
